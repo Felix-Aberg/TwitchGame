e6686c9eb386976b88e154a87cf4f9c6be142203
Simon <ostman678@gmail.com>
2020-10-01 11:47:49 +0200

    Death

--- Assets/Scripts/BallScripts/Ball.cs
+++ Assets/Scripts/BallScripts/Ball.cs
@@ -9,6 +9,8 @@ public class Ball : MonoBehaviour
     BallCollision ballCollision;
     public float minimumRPM;
 
+    public ParticleSystem deathParticles;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,6 +41,7 @@ public class Ball : MonoBehaviour
 
         //TODO: explode
 
+        deathParticles.Play();
         Destroy(gameObject);
     }
 }
