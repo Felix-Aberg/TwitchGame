9f50610cafa431186a6425f5816efebfd38e8300
Erik Norstedt <eriknorth.swe@gmail.com>
2020-11-04 16:49:50 +0100

    Pusher is buggy as heck

---
+++ Assets/Blastof.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blastof : MonoBehaviour
+{
+    public Vector3 targethight;
+    public float upSpeed;
+    public float downSpeed;
+   public Vector3 startpos;
+    public bool go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < startpos.y)
+        {
+            go = true;
+        }
+        if(go == true)
+        {
+        transform.position = transform.position + new Vector3(0, 5 * upSpeed * Time.deltaTime, 0);
+
+        }
+        if(transform.position.y > targethight.y)
+        {
+            go = false;
+        }
+        if (go == false)
+        {
+            transform.position = transform.position - new Vector3(0, 5 * downSpeed * Time.deltaTime, 0);
+        }
+
+        
+    }
+}
