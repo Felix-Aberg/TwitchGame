630e39f2c9d3e7b0347cc036afa6d5ccd6a2fc81
Erik Norstedt <eriknorth.swe@gmail.com>
2020-11-05 14:38:13 +0100

    Launcher and LauncherLevel

--- Assets/Blastof.cs
+++ Assets/Blastof.cs
@@ -10,30 +10,49 @@ public class Blastof : MonoBehaviour
    public Vector3 startpos;
     public bool go;
 
+    public float MinTimer;
+    public float MaxTimer;
+    [SerializeField]
+    float timerr;
+
+    public GameObject blasteroffer;
+
     // Start is called before the first frame update
     void Start()
     {
+        timerr = Random.Range(MinTimer, MaxTimer);
         startpos = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < startpos.y)
+        if(transform.position.y <= startpos.y)
         {
-            go = true;
+
+            timerr -= Time.deltaTime;
+            if(timerr <= 0)
+            {
+                go = true;
+
+                timerr = Random.Range(MinTimer, MaxTimer);
+            }
         }
         if(go == true)
         {
-        transform.position = transform.position + new Vector3(0, 5 * upSpeed * Time.deltaTime, 0);
+            blasteroffer.GetComponent<Collider>().enabled = true;
+            transform.position = transform.position + new Vector3(0, 5 * upSpeed * Time.deltaTime, 0);
 
         }
         if(transform.position.y > targethight.y)
         {
             go = false;
         }
-        if (go == false)
+        if (go == false && transform.position.y > startpos.y)
         {
+            blasteroffer.GetComponent<Collider>().enabled = false;
+
             transform.position = transform.position - new Vector3(0, 5 * downSpeed * Time.deltaTime, 0);
         }
 
