378a8a5889fbf2648aca455d8ff98350a7203bbb
Simon <ostman678@gmail.com>
2020-12-02 15:59:49 +0100

    Exit, simon wip, text

---
+++ Assets/Scripts/Other/ExitApp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApp : MonoBehaviour
+{
+    public void doExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exited GameApp");
+    }
+}
