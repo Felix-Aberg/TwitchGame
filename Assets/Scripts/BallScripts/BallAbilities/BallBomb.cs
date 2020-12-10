96b0b7660dd19360167b8ef04b0ad958c881e752
Felix Åberg <felix.aberg.2773@student.uu.se>
2020-12-10 19:03:43 +0100

    Ball command changes, ability charges

---
+++ Assets/Scripts/BallScripts/BallAbilities/BallBomb.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This ability is the bomb spawned by the ball ability 
+/// </summary>
+public class BallBomb : MonoBehaviour
+{
+    string originPlayer;
+    float explosionTimer;
+    float explosionDamage;
+    float explosionRange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Pass the values from the previous bomb script
+    /// </summary>
+    void PassValues(float timer, string origin)
+    {
+        explosionTimer = timer;
+        originPlayer = origin;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        explosionTimer -= Time.fixedDeltaTime;
+        if (explosionTimer < 0f)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        
+    }
+}
