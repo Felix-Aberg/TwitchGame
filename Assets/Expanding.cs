96df09c21dcad60b8faaf56969893d1a01806ef3
Erik Norstedt <eriknorth.swe@gmail.com>
2020-12-14 10:23:48 +0100

    lazer ship

---
+++ Assets/Expanding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expanding : MonoBehaviour
+{
+
+    public GameObject shpere;
+    public Vector3 scaleChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shpere.transform.localScale += scaleChange;
+    }
+}
